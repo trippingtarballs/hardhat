5e560cce36e017328cc8f8c56881341753aebf8b
Antony Blakey <antony.blakey@gmail.com>
2024-08-14 15:43:24 +0000

    start of structuring

--- v-next/resolver/src/dependency-graph-builder.ts
+++ v-next/resolver/src/dependency-graph-builder.ts
@@ -6,6 +6,11 @@ import { NonterminalKind } from "@nomicfoundation/slang/kinds/index.js";
 import { Language } from "@nomicfoundation/slang/language/index.js";
 import { Query } from "@nomicfoundation/slang/query/index.js";
 
+// Maybe this should all be in the Project instance, to avoid startup cost?
+const supportedVersions = Language.supportedVersions();
+const mostRecentVersion = supportedVersions[supportedVersions.length - 1];
+const language = new Language(mostRecentVersion);
+
 const importQuery =
     `(
        [PathImport @path path: [_]]
@@ -18,89 +23,142 @@ const pragmaQuery =
 
 const queries = [importQuery, pragmaQuery].map(Query.parse);
 
-const supportedVersions = Language.supportedVersions();
-const mostRecentVersion = supportedVersions[supportedVersions.length - 1];
-const language = new Language(mostRecentVersion);
+type SourceName = string;
+type Version = string; // actually, no - find something better, from semver most likely
 
-const unvisitedSourceNames = new Array<string>();
 interface Source {
-    dependencies: Set<string>;
+    dependencies: Set<SourceName>;
     compatibleVersions: BitSet.default;
 }
-const graph = new Map<string, Source>();
 
-{
-    let sourceName;
-    while ((sourceName = unvisitedSourceNames.pop()) !== undefined) {
-        const source = graph.get(sourceName);
-        assertHardhatInvariant(source !== undefined, "We have already added this source to the graph");
-
-        const contents = ""; // TODO: read the file (failure is a fatal error)
-        const output = language.parse(NonterminalKind.SourceUnit, contents);
-        const cursor = output.createTreeCursor();
-        const matches = cursor.query(queries);
-
-        let match;
-        while ((match = matches.next()) !== null) {
-            if (match.queryNumber === 0) {
-                const imprt = match.captures.path[0].node.toString();
-                // TODO: ensure imprt is a direct import
-                // TODO: resolve imprt according to the context and remappings
-                source.dependencies.add(imprt);
-                ensureImportIsProcessed(imprt);
-            } else {
-                // VersionExpressionSets are the disjunction of VersionExpressions
-                const bitset = match.captures.versionExpression.map(bitsetFromVersionExpression).reduce((a, b) => a.or(b));
-                source.compatibleVersions = source.compatibleVersions.and(bitset);
-            }
-        }
-    }
+export interface Remapping {
+    context: string;
+    prefix: string;
+    target: string;
 }
 
-function ensureImportIsProcessed(imprt: string) {
-    if (!graph.has(imprt)) {
-        graph.set(imprt, { dependencies: new Set(), compatibleVersions: new BitSet.default(supportedVersions.length).flip() });
-        unvisitedSourceNames.push(imprt);
+export interface Root {
+    dependencies: Set<SourceName>;
+    bestVersion?: Version;
+}
+
+export class Project {
+
+    readonly #allowableVersions: Version[]; // sorted in descending order
+    readonly #remappings: Remapping[];
+
+    readonly #sources = new Map<SourceName, Source>();
+    readonly #roots = new Map<SourceName, Root>();
+
+    constructor(allowableVersions: Version[], remappings: Remapping[]) {
+        this.#allowableVersions = allowableVersions;
+        this.#remappings = remappings;
     }
+
+    public createRoot(rootSourceName: SourceName): Root {
+        // maybe this should mark the root as changed if we already have it?
+
+        if (!this.#roots.has(rootSourceName)) {
+
+            const unvisitedSourceNames = new Array<string>();
+
+            const ensureSourceNameIsProcessed = (sourceName: SourceName) => {
+                if (!this.#sources.has(sourceName)) {
+                    this.#sources.set(sourceName, { dependencies: new Set(), compatibleVersions: new BitSet.default().flip() });
+                    unvisitedSourceNames.push(sourceName);
+                }
+            }
+
+
+            {
+                // Add the root and it's dependencies to the graph of sources
+
+                ensureSourceNameIsProcessed(rootSourceName);
+
+                let sourceName;
+                while ((sourceName = unvisitedSourceNames.pop()) !== undefined) {
+                    const source = this.#sources.get(sourceName);
+                    assertHardhatInvariant(source !== undefined, "We have already added this source to the graph");
+
+                    const contents = this.#getSourceContent(sourceName);
+                    const output = language.parse(NonterminalKind.SourceUnit, contents);
+                    const cursor = output.createTreeCursor();
+                    const matches = cursor.query(queries);
+
+                    let match;
+                    while ((match = matches.next()) !== null) {
+                        if (match.queryNumber === 0) {
+                            const importSourceName = this.#resolveImport(sourceName, match.captures.path[0].node.toString());
+                            // TODO: what if the import doesn't exist?
+                            source.dependencies.add(importSourceName);
+                            ensureSourceNameIsProcessed(importSourceName);
+                        } else {
+                            // VersionExpressionSets are the disjunction of VersionExpressions
+                            const compatibleVersions = match.captures.versionExpression.map(bitsetFromVersionExpression).reduce((a, b) => a.or(b));
+                            source.compatibleVersions = source.compatibleVersions.and(compatibleVersions);
+                        }
+                    }
+                }
+            }
+
+            {
+                // Compute the transitive dependencies and version constraints of the root
+
+                const dependencies = new Set<string>();
+                const compatibleVersions = new BitSet.default(supportedVersions.length).flip();
+
+                const visit = (sourceName: SourceName) => {
+                    const source = this.#sources.get(sourceName);
+                    assertHardhatInvariant(source !== undefined, "We have already added this source to the graph");
+                    for (const dependency of source.dependencies) {
+                        if (!dependencies.has(dependency)) {
+                            dependencies.add(dependency);
+                            visit(dependency);
+                        }
+                        compatibleVersions.and(source.compatibleVersions);
+                    }
+                }
+
+                visit(rootSourceName);
+
+                // Determine the best version from the allowable versions that satisfies the transitive constraints
+
+                let bestVersion;
+                for (let i = supportedVersions.length - 1; i >= 0; i--) {
+                    if (compatibleVersions.get(i) === 1) {
+                        const candidateVersion = supportedVersions[i];
+                        if (this.#allowableVersions.includes(candidateVersion)) {
+                            bestVersion = candidateVersion
+                            break;
+                        }
+                    }
+                }
+
+                // if bestVersion is undefined, the versions constraints are unsatisfiable
+
+                this.#roots.set(rootSourceName, { dependencies, bestVersion });
+            }
+        }
+
+        const root = this.#roots.get(rootSourceName);
+        assertHardhatInvariant(root !== undefined, "We have already added this root to the set of roots");
+        return root;
+    }
+
+    #getSourceContent(_sourceName: SourceName): string {
+        // TODO: read the file (failure is a fatal error)
+        return "";
+    }
+
+    #resolveImport(_context: SourceName, _importPath: string): SourceName {
+        // TODO: convert a relative import to a direct import
+        // TODO: resolve the import according to the context and this.#remappings (Pato's code)
+        return "";
+    }
+
 }
 
 function bitsetFromVersionExpression(_expr: Cursor) {
-    // TODO: implement
+    // TODO: implement this
     return new BitSet.default();
 };
-
-interface Root {
-    dependencies: Set<string>;
-    bestVersion?: string;
-}
-const roots = new Map<string, Root>();
-
-function _addRoot(rootSourceName: string) {
-    const dependencies = new Set<string>();
-    const compatibleVersions = new BitSet.default(supportedVersions.length).flip();
-
-    const visit = (sourceName: string) => {
-        const source = graph.get(sourceName);
-        assertHardhatInvariant(source !== undefined, "We have already added this source to the graph");
-        for (const dependency of source.dependencies) {
-            if (!dependencies.has(dependency)) {
-                dependencies.add(dependency);
-                visit(dependency);
-            }
-            compatibleVersions.and(source.compatibleVersions);
-        }
-    }
-
-    visit(rootSourceName);
-
-    let bestVersion;
-    for (let i = supportedVersions.length - 1; i >= 0; i--) {
-        if (compatibleVersions.get(i) === 1) {
-            // - TODO: select a compatible version from the allowed versions (failure is a fatal error)
-            bestVersion = supportedVersions[i];
-            break;
-        }
-    }
-
-    roots.set(rootSourceName, { dependencies, bestVersion });
-}
