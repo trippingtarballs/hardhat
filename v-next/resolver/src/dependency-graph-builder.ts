692dd4bcb3789317c787f0518e6e92fbc441aed4
Antony Blakey <antony.blakey@gmail.com>
2024-08-14 05:51:49 +0000

    First pass of process

--- v-next/resolver/src/dependency-graph-builder.ts
+++ v-next/resolver/src/dependency-graph-builder.ts
@@ -1,17 +1,106 @@
-// initialize the unvisited source names with the roots, which are source names
-// while there are unvisited source names
-// - add it to the graph
-// - get and parse the source (failure is a fatal error)
-// - collect the direct imports
-//   - convert each import to a source name using the context and remappings
-//   - add the source name as a dependency of the current source name
-//   - if the source name is not in the graph, add it to the unvisited set
-// - collect the version pragmas
-//   - covert each pragma to an interval representation
-//   - form the conjunction of those intervals
-//   - attach that to the source name in the graph
+import type { Cursor } from "@nomicfoundation/slang/cursor/index.js";
 
-// for each root
-// - determine the conjunction of its dependent's version intervals
-// - select a compatible version from the allowed versions (failure is a fatal error)
-// - store this with the root as the 'root version'
+import { assertHardhatInvariant } from "@ignored/hardhat-vnext-errors";
+import BitSet from "@marsraptor/bitset";
+import { NonterminalKind } from "@nomicfoundation/slang/kinds/index.js";
+import { Language } from "@nomicfoundation/slang/language/index.js";
+import { Query } from "@nomicfoundation/slang/query/index.js";
+
+const importQuery =
+    `(
+       [PathImport @path path: [_]]
+     | [NamedImport @path path: [_]]
+     | [ImportDeconstruction @path path: [_]]
+     )`;
+
+const pragmaQuery =
+    `[VersionPragma [VersionExpressionSets (@versionExpression [VersionExpression])+]]`;
+
+const queries = [importQuery, pragmaQuery].map(Query.parse);
+
+const supportedVersions = Language.supportedVersions();
+const mostRecentVersion = supportedVersions[supportedVersions.length - 1];
+const language = new Language(mostRecentVersion);
+
+const unvisitedSourceNames = new Array<string>();
+interface Source {
+    dependencies: Set<string>;
+    compatibleVersions: BitSet.default;
+}
+const graph = new Map<string, Source>();
+
+{
+    let sourceName;
+    while ((sourceName = unvisitedSourceNames.pop()) !== undefined) {
+        const source = graph.get(sourceName);
+        assertHardhatInvariant(source !== undefined, "We have already added this source to the graph");
+
+        const contents = ""; // TODO: read the file (failure is a fatal error)
+        const output = language.parse(NonterminalKind.SourceUnit, contents);
+        const cursor = output.createTreeCursor();
+        const matches = cursor.query(queries);
+
+        let match;
+        while ((match = matches.next()) !== null) {
+            if (match.queryNumber === 0) {
+                const imprt = match.captures.path[0].node.toString();
+                // TODO: ensure imprt is a direct import
+                // TODO: resolve imprt according to the context and remappings
+                source.dependencies.add(imprt);
+                ensureImportIsProcessed(imprt);
+            } else {
+                // VersionExpressionSets are the disjunction of VersionExpressions
+                const bitset = match.captures.versionExpression.map(bitsetFromVersionExpression).reduce((a, b) => a.or(b));
+                source.compatibleVersions = source.compatibleVersions.and(bitset);
+            }
+        }
+    }
+}
+
+function ensureImportIsProcessed(imprt: string) {
+    if (!graph.has(imprt)) {
+        graph.set(imprt, { dependencies: new Set(), compatibleVersions: new BitSet.default(supportedVersions.length).flip() });
+        unvisitedSourceNames.push(imprt);
+    }
+}
+
+function bitsetFromVersionExpression(_expr: Cursor) {
+    // TODO: implement
+    return new BitSet.default();
+};
+
+interface Root {
+    dependencies: Set<string>;
+    bestVersion?: string;
+}
+const roots = new Map<string, Root>();
+
+function _addRoot(rootSourceName: string) {
+    const dependencies = new Set<string>();
+    const compatibleVersions = new BitSet.default(supportedVersions.length).flip();
+
+    const visit = (sourceName: string) => {
+        const source = graph.get(sourceName);
+        assertHardhatInvariant(source !== undefined, "We have already added this source to the graph");
+        for (const dependency of source.dependencies) {
+            if (!dependencies.has(dependency)) {
+                dependencies.add(dependency);
+                visit(dependency);
+            }
+            compatibleVersions.and(source.compatibleVersions);
+        }
+    }
+
+    visit(rootSourceName);
+
+    let bestVersion;
+    for (let i = supportedVersions.length - 1; i >= 0; i--) {
+        if (compatibleVersions.get(i) === 1) {
+            // - TODO: select a compatible version from the allowed versions (failure is a fatal error)
+            bestVersion = supportedVersions[i];
+            break;
+        }
+    }
+
+    roots.set(rootSourceName, { dependencies, bestVersion });
+}
